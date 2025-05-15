0737a7fa7d09cb578885e54a0acdc18947a37661
ehsanghannad <ghannad73@gmail.com>
2025-05-15 21:59:00 +0330

    add some css in input comp and useReduce

--- CodeLearn-Shop/src/components/Form/Input.jsx
+++ CodeLearn-Shop/src/components/Form/Input.jsx
@@ -1,22 +1,48 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./Input.css";
 
 export default function Input(props) {
+  const inputReducer = (state, action) => {
+    switch (action.type) {
+      case "CHANGE": {
+        return {
+          ...state,
+          value: action.value,
+          isValid: action.isValid,
+        };
+      }
+
+      default: {
+        return state;
+      }
+    }
+  };
+
+  const [mainInput, dispatch] = useReducer(inputReducer, {
+    value: "",
+    isValid: false,
+  });
+
   const onChangeHandler = (event) => {
     console.log(event.target.value);
+    dispatch({ type: "CHANGE", value: event.target.value, isValid: true });
   };
   const element =
     props.element === "input" ? (
       <input
         type={props.type}
         placeholder={props.placeholder}
-        className={props.className}
+        className={`${props.className} ${
+          mainInput.isValid === true ? "success" : "error"
+        }`}
+        value={mainInput.value}
         onChange={onChangeHandler}
       />
     ) : (
       <textarea
         placeholder={props.placeholder}
         className={props.className}
+        value={mainInput.value}
         onChange={onChangeHandler}
       />
     );
