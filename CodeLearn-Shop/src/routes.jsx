cdd2a2930867dcfe4f60b0f8ac155ea980ab74d3
ehsanghannad <ghannad73@gmail.com>
2025-05-15 20:18:16 +0330

    add login and signup pages

--- CodeLearn-Shop/src/routes.jsx
+++ CodeLearn-Shop/src/routes.jsx
@@ -3,6 +3,8 @@ import CourseInfo from "./pages/CourseInfo/CourseInfo";
 import Category from "./pages/Category/Category";
 import Article from "./pages/Article/Article";
 import Courses from "./pages/Courses/Courses";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
 
 const routes = [
   { path: "/", element: <Index /> },
@@ -10,6 +12,8 @@ const routes = [
   { path: "/category/:categoryName", element: <Category /> },
   { path: "/article/:articleName", element: <Article /> },
   { path: "/courses/:coursesName", element: <Courses /> },
+  { path: "/login", element: <Login /> },
+  { path: "/signup", element: <Signup /> },
 ];
 
 export default routes;
